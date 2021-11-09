3e439ebc189cb01cfa4f493b5269cdbbad8000dd
aminiyolo <73641636+aminiyolo@users.noreply.github.com>
2021-11-09 18:32:17 +0900

    context adding progress

--- react-test-practice/src/pages/OrderPage/Type.js
+++ react-test-practice/src/pages/OrderPage/Type.js
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Products from "./Products";
 import axios from "axios";
 import ErrorBanner from "../../components/ErrorBanner";
 import Options from "./Options";
+import { OrderContext } from "../../contexts/OrderContext";
 
 const Type = ({ orderType }) => {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+
+  const [orderDatas, updateItemCount] = useContext(OrderContext);
+  console.log(orderDatas);
 
   const loadItems = async (orderType) => {
     try {
@@ -28,14 +32,21 @@ const Type = ({ orderType }) => {
   const ItemComponents = orderType === "products" ? Products : Options;
 
   const optionItems = items.map((item) => (
-    <ItemComponents key={item.name} name={item.name} imagePath={""} />
+    <ItemComponents
+      key={item.name}
+      name={item.name}
+      imagePath={""}
+      updateItemCount={(itemName, newItemCount) =>
+        updateItemCount(itemName, newItemCount, orderType)
+      }
+    />
   ));
 
   return (
     <>
       <h2>주문 종류</h2>
       <p>하나의 가격</p>
-      <p>총 가격</p>
+      <p>총 가격: {orderDatas.totals[orderType]}</p>
 
       <div
         style={{
--- react-test-practice/src/pages/OrderPage/tests/Calculate.test.js
+++ react-test-practice/src/pages/OrderPage/tests/Calculate.test.js
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Type from "../Type";
+import { OrderContextProvider } from "../../../contexts/OrderContext";
 
 test("update product's total when products quantity change", async () => {
-  render(<Type orderType="products" />);
+  render(<Type orderType="products" />, { wrapper: OrderContextProvider });
 
-  const productsTotal = screen.getByText("상품 총 가격:", { exact: false });
-  expect(productsTotal).toHaveText("0");
+  const productsTotal = screen.getByText("총 가격:", { exact: false });
+  expect(productsTotal).toHaveTextContent("0");
 
   // 상품 하나 추가 시
   const americaInput = await screen.findByRole("spinbutton", {
@@ -15,5 +16,5 @@ test("update product's total when products quantity change", async () => {
 
   userEvent.clear(americaInput);
   userEvent.type(americaInput, "1");
-  expect(productsTotal).toHaveText("1000");
+  expect(productsTotal).toHaveTextContent("1000");
 });
